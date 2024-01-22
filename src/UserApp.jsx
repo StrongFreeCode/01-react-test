28659c849f7bbfa0764144e9253a103dfe8ced97
theone8907 <franklincampos8907@gmail.com>
2024-01-22 04:39:17 -0500

    realizado validacion de los estados

--- src/UserApp.jsx
+++ src/UserApp.jsx
@@ -1,37 +1,20 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { ListUser } from "./components/ListUser"
 
 export const UserApp = () => {
-    const [users, setUsers] = useState([])
-
-    const fetchUsers = async () => {
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users')
-            const data = await response.json()
-            console.log(data)
-            setUsers(data)
-           
-        } catch (error) {
-            console.error(error)
-        }
-        
-    }
-   /*  useEffect(() => {
-        fetchUsers()
-    }, []) */
+    
+    const [endPoint, setendPoint] = useState('users')
     const handleClick = () => {
-        fetchUsers()
+        setendPoint(endPoint == 'users' ? 'comments'
+        : 'users')
     }
     return (
         <>
-            <div className="flex-1">
-                <h1 className="text-3xl font-bold underline">Lista de Usuarios:</h1>
-                <ul>
-                    {users.map(user => <li key={user.id}><span className="text-gray-300">Nombre:</span> {user.name} <span className="text-gray-300">email:</span>{ user.email}</li>)}     
-                </ul>
-            </div>
             <div>
-                <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Actualizar</button>
+                <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Mostrar {endPoint == 'users'? 'commetarios': 'usuarios'}</button>
             </div>
+            
+            <ListUser endPoint={endPoint}/>
         </>
   )
 }
--- src/components/ListUser.jsx
+++ src/components/ListUser.jsx
@@ -1,5 +1,32 @@
-export const ListUser = () => {
+import { useEffect, useState } from "react"
+
+export const ListUser = ({ endPoint }) => {
+    const [data, setData] = useState([])
+
+    const fecthdata = async () => {
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/${endPoint}`)
+            const data = await response.json()
+            console.log(data)
+            setData(data)
+           
+        } catch (error) {
+            console.error(error)
+        }
+        
+    }
+    useEffect(() => {
+        fecthdata()
+    }, [endPoint])
   return (
-    <div>ListUser</div>
+      <>
+          <div className="flex-1">
+                <h1 className="text-3xl font-bold underline">Lista de Usuarios:</h1>
+                <ul>
+                  {endPoint == 'users' ? data.map(item => <li key={item.id}><span className="text-gray-300">Nombre:</span> {item.name} <span className="text-gray-300">email:</span>{item.email}</li>)
+                                       : data.map(item => <li key={item.id}><span className="text-gray-300">Nombre:</span> {item.name} <span className="text-gray-300">Comment:</span>{ item.body}</li>)}     
+                </ul>
+            </div>
+      </>
   )
 }

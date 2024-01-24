9eedcf2fb2ec45073271c5bcb8b8102defb556f5
theone8907 <franklincampos8907@gmail.com>
2024-01-23 22:19:49 -0500

    custom hook y helpers

--- src/components/ListUser.jsx
+++ src/components/ListUser.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react"
 import { useFetchData } from "./hook/useFetchData"
 
 export const ListUser = ({ endPoint }) => {
-  const {data, isloading} = useFetchData(endPoint)
+  const {data, isLoading} = useFetchData(endPoint)
   return (
       <>
           <div className="flex-1">
                 <h1 className="text-3xl font-bold underline">Lista de Usuarios:</h1>
                 <ul>
-                  {endPoint == 'users' ? data.map(item => <li key={item.id}><span className="text-gray-300">Nombre:</span> {item.name} <span className="text-gray-300">email:</span>{item.email}</li>)
+                  {isLoading 
+                  ? <p>Cargando...</p> 
+                  :endPoint == 'users' ? data.map(item => <li key={item.id}><span className="text-gray-300">Nombre:</span> {item.name} <span className="text-gray-300">email:</span>{item.email}</li>)
                                        : data.map(item => <li key={item.id}><span className="text-gray-300">Nombre:</span> {item.name} <span className="text-gray-300">Comment:</span>{ item.body}</li>)}     
                 </ul>
             </div>
